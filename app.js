3ddd5bb59b0fa2e7717a8d281741bac2d7dfcd45
Rohan Gupta <guptarohan369@gmail.com>
2019-01-05 17:15:51 +0000

    fix file upload system

--- app.js
+++ app.js
@@ -2,25 +2,33 @@ require('dotenv').config();
 
 let express     = require("express"),
     app         = express(),
+    fs          = require("fs"),
     mongoose    = require("mongoose"),
     bodyParser  = require("body-parser"),
-    jsSHA       = require("jssha");
+    jsSHA       = require("jssha"),
+    FileReader  = require("filereader"),
+    sha256      = require("sha256"),
+    multer      = require("multer");
     
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public")); 
+
 mongoose.connect("mongodb://localhost/doc-checker", {useNewUrlParser: true});
 
 let docSchema = new mongoose.Schema({
     name: String,
     roll: String,
+    hash_data: String,
     college: String,
-    college_id: String,
-    file: String
+    college_id: String
 });
 
 let Degree = mongoose.model("Degree", docSchema);
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));   
+var upload = multer({ dest: 'public/documents/' });
 
 app.get("/", function(req, res){
     res.render("index");
@@ -30,40 +38,56 @@ app.get("/upload", function(req, res){
     res.render("upload");
 });
 
-app.post("/", function(req, res){
+app.post("/upload", upload.any(), function(req, res){
+//   console.log(req.files[0]);
+   
    let name = req.body.name;
    let roll = req.body.roll;
    let college = req.body.college;
    let college_id = req.body.college_id;
    let file;
+   let hash_data;
+   let newDoc;
    
-   let myFile = req.body.file;
-   
-   var reader = new FileReader();
-   reader.onload = function() {
-       var actual_contents = reader.result.slice(reader.result.indexOf(',') + 1);
-       var what_i_need = new jsSHA(actual_contents, "B64").getHash("SHA-256", "HEX");
-   };
-   file = reader.readAsDataURL(myFile);
-   
-   let newDoc = {
-       name: name,
-       roll: roll,
-       college: college,
-       college_id: college_id,
-       file: file
-   };
-   
-   Degree.create(newDoc, function(err, createdDoc){
-       if (err){
-           console.log(err);
-           return res.redirect("back");
-       }
-       else{
-           console.log(createdDoc);
-           res.redirect("/");
-       }
-   });
+   fs.rename(req.files[0].path, req.files[0].destination + req.files[0].originalname, function(err){
+        if (err){
+           return console.log(err);
+        }
+        else{
+           console.log("File renamed!");
+           file = req.files[0].destination + req.files[0].originalname;
+           
+            fs.readFile(file, function(err, data){
+                if (err){
+                    return console.log(err);
+                }
+                else{
+                    hash_data = sha256(data);
+                    // console.log(hash_data);
+                    newDoc = {
+                       name: name,
+                       roll: roll,
+                       hash_data: hash_data,
+                       college: college,
+                       college_id: college_id
+                    };
+                   
+                    console.log(newDoc);
+                   
+                    Degree.create(newDoc, function(err, createdDoc){
+                        if (err){
+                           console.log(err);
+                           return res.redirect("back");
+                        }
+                        else{
+                        //   console.log(createdDoc);
+                           res.redirect("/");
+                        }
+                    });
+                }
+            });
+        }
+    });
 });
 
 app.get("/verify", function(req, res){
@@ -76,24 +100,29 @@ app.post("/verify", function(req, res){
    let college = req.body.college;
    let college_id = req.body.college_id;
    let file;
+   let hash_data;
+   let newDoc;
    
-   let myFile = req.body.file;
-   
-   var reader = new FileReader();
-   reader.onload = function() {
-       var actual_contents = reader.result.slice(reader.result.indexOf(',') + 1);
-       var what_i_need = new jsSHA(actual_contents, "B64").getHash("SHA-256", "HEX");
-   };
-   file = reader.readAsDataURL(myFile);
-   
-   let docObj = {
-       name: name,
-       roll: roll,
-       college: college,
-       college_id: college_id,
-       file: file
-   }
-   
+   fs.rename(req.files[0].path, req.files[0].destination + req.files[0].originalname, function(err){
+        if (err){
+           return console.log(err);
+        }
+        else{
+           console.log("File renamed!");
+           file = req.files[0].destination + req.files[0].originalname;
+           
+            fs.readFile(file, function(err, data){
+                if (err){
+                    return console.log(err);
+                }
+                else{
+                    hash_data = sha256(data);
+                    // console.log(hash_data);
+                    // PUT SCRIPT HERE
+                }
+            });
+        }
+   });
 });
 
 app.get("*", function(req, res){
