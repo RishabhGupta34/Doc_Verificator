0160cc7e082a49d5ba4c197ced2c46dcb1d75a42
Rohan Gupta <guptarohan369@gmail.com>
2019-01-05 06:45:35 +0000

    add upload.ejs and verify.ejs

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ let express     = require("express"),
     mongoose    = require("mongoose"),
     bodyParser  = require("body-parser");
     
+mongoose.connect("mongodb://localhost/doc-checker", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));   
 
 app.get("/", function(req, res){
     res.render("index");
+});
+
+app.get("/upload", function(req, res){
+    res.render("upload");
+});
+
+app.get("/verify", function(req, res){
+    res.render("verify");
 });
 
 app.get("*", function(req, res){
