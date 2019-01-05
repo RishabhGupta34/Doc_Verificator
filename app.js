0d06e71945b0d3a0f13fd2a2bb37257be1d42a65
Rohan Gupta <guptarohan369@gmail.com>
2019-01-05 05:52:39 +0000

    add index.ejs

--- app.js
+++ app.js
@@ -5,7 +5,18 @@ let express     = require("express"),
     mongoose    = require("mongoose"),
     bodyParser  = require("body-parser");
     
-    
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));   
+
+app.get("/", function(req, res){
+    res.render("index");
+});
+
+app.get("*", function(req, res){
+    res.redirect("back");
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Server started");
-})
+    console.log("Server started...");
+});
