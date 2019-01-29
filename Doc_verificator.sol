d2d85add3a7f8d132342213526c1ac96b0f8beb0
RishabhGupta34 <rgupta2_be16@thapar.edu>
2019-01-29 16:35:35 +0530

    Minor changes

--- Doc_verificator.sol
+++ Doc_verificator.sol
@@ -1,46 +1,20 @@
 pragma solidity ^0.4.24;
 contract Doc_verificator{
     struct student{
-        string s_name;
-        string s_id;
-        string c_id;
-        string doc_hash;
-        string c_name;
+        mapping (int => string) stoc;
     }
-    function strConcat(string memory _a, string memory _b) public returns (string memory){ 
-    bytes memory _ba = bytes(_a); 
-    bytes memory _bb = bytes(_b);
-    string memory abcde = new string(_ba.length + _bb.length); 
-    bytes memory concatstr = bytes(abcde); 
-    uint k = 0; 
-    for (uint i = 0; i < _ba.length; i++) concatstr[k++] = _ba[i]; 
-    for (uint i = 0; i < _bb.length; i++) concatstr[k++] = _bb[i]; 
-    return string(concatstr); 
+    mapping (int => student) students;
+    function newstudent (int _s_id,int _c_id,string memory _doc_hash) public{
+        students[_s_id].stoc[_c_id]=_doc_hash;
     } 
-    function stringsEqual(string memory _a, string memory _b) public returns (bool) {
-		bytes memory a = bytes(_a);
-		bytes memory b = bytes(_b);
-		if (a.length != b.length)
-			return false;
-		for (uint i = 0; i < a.length; i ++)
-			if (a[i] != b[i])
-				return false;
-		return true;
-	}
-    mapping (string => student) private stud;
-    function newstudent (string memory _s_name,string memory _s_id,string memory _c_id,string memory _doc_hash,string memory _c_name) public{
-        string memory _index=strConcat(_s_id,_c_id);
-        stud[_index].s_name=_s_name;
-        stud[_index].s_id=_s_id;
-        stud[_index].c_id=_c_id;
-        stud[_index].c_name=_c_name;
-        stud[_index].doc_hash=_doc_hash;
-    } 
-    function getstud (string memory _s_id,string memory _c_id,string memory _doc_hash) public returns (string memory){
-        string memory _index=strConcat(_s_id,_c_id);
-        if(stringsEqual(stud[_index].doc_hash,_doc_hash)){
-            return ("true");}
-        else{
-            return ("false");}
-    }
+    function getstud (int _s_id,int _c_id,string memory _doc_hash) view public returns (bool){
+        bytes memory a = bytes(students[_s_id].stoc[_c_id]);
+        bytes memory b = bytes(_doc_hash);
+        if (a.length != b.length)
+            return false;
+        for (uint i = 0; i < a.length; i ++)
+            if (a[i] != b[i])
+                return false;
+        return true;
+}
 }
--- app.js
+++ app.js
@@ -9,62 +9,39 @@ let express     = require("express"),
     FileReader  = require("filereader"),
     sha256      = require("sha256"),
     multer      = require("multer");
-    
-var Web3=require('web3'); 
- // set the provider you want from Web3.providers
-var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/cbfbd8fd2d124b66890e2c0ad4f0ebd3'));
 
-web3.eth.defaultAccount = web3.eth.accounts[0];
-console.log(web3.eth.accounts[0]);
+const MetaMaskConnector = require('node-metamask');
+// const connector = new MetaMaskConnector({
+//   port: 8000, // this is the default port
+//   onConnect() { console.log('MetaMask client connected') }, // Function to run when MetaMask is connected (optional)
+// });  
+var Web3=require('web3'); 
+// var web3 = new Web3();
+ // set the provider you want from Web3.providers
+// connector.start().then(() => {
+//   // Now go to http://localhost:3333 in your MetaMask enabled web browser.
+//   const web3 = new Web3(connector.getProvider());
+//   // Use web3 as you would normally do. Sign transactions in the browser.
+// });				
+// web3 = new Web3(web3.setProvider(web3.currentProvider));
+var web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'));
+// web3.eth.defaultAccount = "0xba9c4d0B37043b1bF0b5c494cEB5e42cBa88B071"
+web3.eth.defaultAccount = web3.eth.accounts[1];
+console.log(web3.eth.defaultAccount);
 var CoursesContract = web3.eth.contract([
 	{
 		"constant": false,
 		"inputs": [
 			{
 				"name": "_s_id",
-				"type": "string"
+				"type": "int256"
 			},
 			{
 				"name": "_c_id",
-				"type": "string"
+				"type": "int256"
 			},
 			{
 				"name": "_doc_hash",
-				"type": "string"
-			}
-		],
-		"name": "getstud",
-		"outputs": [
-			{
-				"name": "",
-				"type": "string"
-			}
-		],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "_s_name",
-				"type": "string"
-			},
-			{
-				"name": "_s_id",
-				"type": "string"
-			},
-			{
-				"name": "_c_id",
-				"type": "string"
-			},
-			{
-				"name": "_doc_hash",
-				"type": "string"
-			},
-			{
-				"name": "_c_name",
 				"type": "string"
 			}
 		],
@@ -75,41 +52,22 @@ var CoursesContract = web3.eth.contract([
 		"type": "function"
 	},
 	{
-		"constant": false,
+		"constant": true,
 		"inputs": [
 			{
-				"name": "_a",
-				"type": "string"
+				"name": "_s_id",
+				"type": "int256"
 			},
 			{
-				"name": "_b",
-				"type": "string"
-			}
-		],
-		"name": "strConcat",
-		"outputs": [
-			{
-				"name": "",
-				"type": "string"
-			}
-		],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "_a",
-				"type": "string"
+				"name": "_c_id",
+				"type": "int256"
 			},
 			{
-				"name": "_b",
+				"name": "_doc_hash",
 				"type": "string"
 			}
 		],
-		"name": "stringsEqual",
+		"name": "getstud",
 		"outputs": [
 			{
 				"name": "",
@@ -117,19 +75,18 @@ var CoursesContract = web3.eth.contract([
 			}
 		],
 		"payable": false,
-		"stateMutability": "nonpayable",
+		"stateMutability": "view",
 		"type": "function"
 	}
 ]);
-var Doc_verificator = CoursesContract.at('0xd690b3e15c53572cb54a63720eae3961e2e3ec1d');
-    
+var Doc_verificator = CoursesContract.at(process.env.CONTRACT_ADDRESS);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public")); 
 
-mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
 
 let docSchema = new mongoose.Schema({
     name: String,
@@ -184,7 +141,7 @@ app.post("/upload", upload.any(), function(req, res){
                        college: college,
                        college_id: college_id
                     };
-                   Doc_verificator.newstudent(name,roll,college_id,hash_data,college);
+                   Doc_verificator.newstudent(roll,college_id,hash_data);
                     console.log(newDoc);
                    
                     Degree.create(newDoc, function(err, createdDoc){
@@ -228,7 +185,11 @@ app.post("/verify", upload.any(), function(req, res){
                 else{
                     hash_data = sha256(data);
                     // console.log(hash_data);
+                    // var tr=Doc_verificator.getstud(roll,college_id,hash_data);
                     console.log(Doc_verificator.getstud(roll,college_id,hash_data));
+                    // var receipt = web3.eth.getTransactionReceipt(tr);
+                    // console.log(receipt);
+                    res.redirect("/");
                 }
             });
         }
@@ -239,6 +200,6 @@ app.get("*", function(req, res){
     res.redirect("back");
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(8000, process.env.IP, function(){
     console.log("Server started...");
 });
